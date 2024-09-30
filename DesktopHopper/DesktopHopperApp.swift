c7af42b331a235f4e71615198de9ef4abce7ea9e
neemspees (Kim Bouchouaram) <kim@libaro.be>
2024-09-30 12:10:16 +0200

    Initial Commit

---
+++ DesktopHopper/DesktopHopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopHopperApp.swift
+//  DesktopHopper
+//
+//  Created by Kim Bouchouaram on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct DesktopHopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
